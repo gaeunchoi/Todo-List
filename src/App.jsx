87fbbe8eeb425ca91111c3765a901d8ca31cec1c
gaeunchoi <gaeun1884@naver.com>
2025-04-23 16:38:48 +0900

    feat: dumpData 생성 + 컴포넌트 연결

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,79 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import ListContent from "./components/ListContent";
+import Header from "./components/Header";
+import { useRef, useState } from "react";
+import CreateItem from "./components/CreateItem";
+
+const dumpData = [
+  {
+    id: 0,
+    isDone: true,
+    content: "Todo-List 제작",
+    date: new Date().getTime(),
+  },
+  { id: 1, isDone: false, content: "청소기 밀기", date: new Date().getTime() },
+  { id: 2, isDone: false, content: "방청소 하기", date: new Date().getTime() },
+  {
+    id: 3,
+    isDone: false,
+    content: "Search test 1",
+    date: new Date().getTime(),
+  },
+  {
+    id: 4,
+    isDone: false,
+    content: "sEARCH TEST 2",
+    date: new Date().getTime(),
+  },
+];
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState(dumpData);
+  let idCnt = useRef(5);
+
+  const handleCreate = (data) => {
+    const newData = {
+      id: idCnt.current++,
+      isDone: false,
+      content: data,
+      date: new Date().getTime(),
+    };
+    setTodos([...todos, newData]);
+  };
+
+  const handleUpdate = (targetId) => {
+    const newTodos = todos.map((todo) =>
+      todo.id === targetId ? { ...todo, isDone: !todo.isDone } : todo
+    );
+
+    setTodos(newTodos);
+  };
+
+  const handleEdit = (targetId, editContent) => {
+    const newTodos = todos.map((todo) =>
+      todo.id === targetId ? { ...todo, content: editContent } : todo
+    );
+
+    setTodos(newTodos);
+  };
+
+  const handleDelete = (targetId) => {
+    const newTodos = todos.filter((todo) => todo.id !== targetId);
+    setTodos(newTodos);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="main-container">
+      <Header />
+      <CreateItem onCreate={handleCreate} />
+      <ListContent
+        todos={todos}
+        onUpdate={handleUpdate}
+        onEdit={handleEdit}
+        onDelete={handleDelete}
+      />
+    </div>
+  );
 }
 
-export default App
+export default App;
